f8e22ed2b9ece444e2e2bad6c3a75d3a027e4e4d
aftertechnologies <mujuniaidan@gmail.com>
2025-08-31 15:40:05 +0300

    accepting command line arguments

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("in file {}", config.file_path);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("should have been able to read this file");
+    println!("WIth text: \n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
+}
